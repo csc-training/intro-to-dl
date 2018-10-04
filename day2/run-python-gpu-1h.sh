5fbc28dbae3714c7c1e7202439790d55a30b5f94
Markus Koskela <markus.koskela@csc.fi>
2018-10-04 12:10:11 +0000

    Add "-c 4"

--- day2/run-python-gpu-1h.sh
+++ day2/run-python-gpu-1h.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -N 1 -p gpu --gres=gpu:p100:1 -t 1:00:00 --mem=8G
+#SBATCH -N 1 -c 4 -p gpu --gres=gpu:p100:1 -t 1:00:00 --mem=8G
 #SBATCH --reservation dlintro
 
 module load python-env/3.5.3-ml
