e1937bb37235c1d8d7a6e23ce5809f144e7c5ed8
Markus Koskela <markus.koskela@csc.fi>
2019-02-06 15:06:21 +0200

    Update run scripts

--- day2/run.sh
+++ day2/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -N 1 -c 4 -p gpu --gres=gpu:p100:1 -t 1:00:00 --mem=8G
+#SBATCH -N 1 -c 4 -p gpu --gres=gpu:k80:1 -t 1:00:00 --mem=8G
 #SBATCH --reservation dlintro
 
-module load python-env/3.5.3-ml
+module load python-env/3.6.3-ml
 module list
 
 set -xv
@@ -11,6 +11,6 @@ date
 hostname
 nvidia-smi
 
-srun python3.5 $*
+srun python3.6 $*
 
 date
