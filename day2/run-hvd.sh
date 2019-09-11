a3511548420cd036b7cabcf4c2548f26f41ec5cf
Markus Koskela <markus.koskela@csc.fi>
2019-09-11 15:26:34 +0300

    Update to use reservation

--- day2/run-hvd.sh
+++ day2/run-hvd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -N 1 -n 4 -c 6 -p gputest --gres=gpu:k80:4 -t 15:00 --mem=32G
-#xSxBxAxTCH --reservation dlintro
+#SBATCH -N 1 -n 2 -c 6 -p gpu --gres=gpu:p100:2 -t 1:00:00 --mem=32G
+#SBATCH --reservation dlintro
 
 module load python-env/3.7.4-ml
 module list
 
 set -xv
-mpirun -np 4 -bind-to none -map-by slot \
+mpirun -np 2 -bind-to none -map-by slot \
     -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
     -mca pml ob1 -mca btl ^openib -oversubscribe \
     python3.7 $*
