edad2c1b76e2c637b0a3368973a19301e750b3fb
Markus Koskela <markus.koskela@csc.fi>
2019-12-10 08:33:57 +0200

    Update horovod run script to Puhti

--- day2/run-hvd.sh
+++ day2/run-hvd.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH -N 1 -n 2 -c 6 -p gpu --gres=gpu:p100:2 -t 1:00:00 --mem=32G
-#SBATCH --reservation dlintro
+#SBATCH -A project_2002238 -N 1 -n 2
+#SBATCH -c 20 -p gpu --gres=gpu:v100:2 -t 1:00:00 --mem=64G
+# # # # #S B A TCH --reservation dlintro
 
-module load python-env/3.7.4-ml
+module load tensorflow/2.0.0-hvd
 module list
 
 set -xv
-mpirun -np 2 -bind-to none -map-by slot \
-    -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
-    -mca pml ob1 -mca btl ^openib -oversubscribe \
-    python3.7 $*
+srun python3.7 $*
+
