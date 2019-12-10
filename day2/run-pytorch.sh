77580f77eebdd7c63ab965e9113c182ce242af91
Markus Koskela <markus.koskela@csc.fi>
2019-12-10 08:37:21 +0200

    Switch to using $LOCAL_SCRATCH

--- day2/run-pytorch.sh
+++ day2/run-pytorch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH -A project_2002238 -c 10 -p gpu --gres=gpu:v100:1 -t 1:00:00 --mem=64G
-#SBATCH --reservation dlintro
+#SBATCH -A project_2002238 -c 10 -p gpu --gres=gpu:v100:1,nvme:100 -t 1:00:00 --mem=64G
+# # # ##SBATCH --reservation dlintro
 
 module load pytorch/1.3.0
 module list
 
 set -xv
-srun python3.7 $*
+echo $TMPDIR $LOCAL_SCRATCH
+export TMPDIR=$LOCAL_SCRATCH
+python3.7 $*
