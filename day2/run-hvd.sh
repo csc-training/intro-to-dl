b4dd8eac92c0163375bd702fa377505682f03bfd
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-12-13 14:38:21 +0200

    Fixed -c 20 => 10 for horovod script

--- day2/run-hvd.sh
+++ day2/run-hvd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A project_2002238 -N 1 -n 2
-#SBATCH -c 20 -p gpu --gres=gpu:v100:2 -t 1:00:00 --mem=64G
+#SBATCH -c 10 -p gpu --gres=gpu:v100:2 -t 1:00:00 --mem=64G
 #SBATCH --reservation dlintro
 
 module load tensorflow/2.0.0-hvd
