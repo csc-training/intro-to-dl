bcc79b9b2876220ebb0fdb43826bcd6b225d3c24
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-12-13 08:43:00 +0200

    Activated reservations

--- day2/run.sh
+++ day2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A project_2002238 -c 10 -p gpu --gres=gpu:v100:1 -t 1:00:00 --mem=64G
-#xxx SBATCH --reservation dlintro
+#SBATCH --reservation dlintro
 
 module load tensorflow/2.0.0
 module list
