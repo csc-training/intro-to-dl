21d67ffbb6436566b06ded1f1497eff24876d701
Thor Wikfeldt <ktwikfeldt@gmail.com>
2021-01-28 14:37:01 +0100

    add run-hvd batch script - but TF-Hvd jobs crash

--- day2/run-hvd.sh
+++ day2/run-hvd.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:v100:2
-#SBATCH --time=1:00:00
-#SBATCH --mem=64G
-#SBATCH --ntasks=2
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
-#SBATCH --account=project_2003747
-#SBATCH --reservation=dlintro
+#SBATCH --gpus-per-node=T4:2
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=2
+#SBATCH -A SNIC2020-5-235
 
-module load tensorflow/nvidia-20.07-tf2-py3
+# For TensorFlow
+module load GCC/8.3.0  CUDA/10.1.243  OpenMPI/3.1.4 TensorFlow/2.1.0-Python-3.7.4
+module load scikit-learn/0.21.3-Python-3.7.4
+module load Horovod/0.19.1-TensorFlow-2.1.0-Python-3.7.4
+
 module list
 
-export DATADIR=/scratch/project_2003747/data
-export KERAS_HOME=/scratch/project_2003747/keras-cache
+tar zxf /cephyr/NOBACKUP/Datasets/Practical_DL/dogs-vs-cats.tar.gz -C $TMPDIR
+#tar zxf /cephyr/NOBACKUP/Datasets/Practical_DL/gtsrb.tar.gz -C $TMPDIR
+#tar zxf /cephyr/NOBACKUP/Datasets/Practical_DL/20_newsgroup.tar.gz -C $TMPDIR
+#tar zxf /cephyr/NOBACKUP/Datasets/Practical_DL/alien-vs-predator-images.zip -C $TMPDIR
+
+export DATADIR=$TMPDIR
+export KERAS_HOME=$TMPDIR/keras-cache
 
 set -xv
-srun singularity_wrapper exec python3 $*
+
+mpirun python $*
