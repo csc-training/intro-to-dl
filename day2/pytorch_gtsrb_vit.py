b47bc2fed032c3d75ff3dafa207b8928b1034303
Mats Sjöberg <mats.sjoberg@csc.fi>
2022-02-08 16:31:04 +0200

    Add pytorch_*_vit scripts. Modified pytorch dvc and gtsrb to be single self-contained scripts

--- day2/pytorch_gtsrb_vit.py
+++ day2/pytorch_gtsrb_vit.py
@@ -3,7 +3,7 @@
 
 # # Traffic sign classification with CNNs
 # 
-# In this notebook, we'll train a convolutional neural network (CNN, ConvNet) to classify images of traffic signs from [The German Traffic Sign Recognition Benchmark](http://benchmark.ini.rub.de/?section=gtsrb&subsection=news) using PyTorch. 
+# In this notebook, we'll finetune a [Vision Transformer](https://arxiv.org/abs/2010.11929) (ViT) to classify images of traffic signs from [The German Traffic Sign Recognition Benchmark](http://benchmark.ini.rub.de/?section=gtsrb&subsection=news) using PyTorch and HuggingFace's [Transformers](https://github.com/huggingface/transformers). 
 # 
 # **Note that using a GPU with this notebook is highly recommended.**
 # 
@@ -14,8 +14,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 from distutils.version import LooseVersion as LV
+
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from transformers import __version__ as transformers_version
+
 from datetime import datetime
 
 if torch.cuda.is_available():
@@ -66,7 +70,6 @@ if local_scratch is not None and not os.path.exists(datapath):
 # 
 # Then, to make the most of our limited number of training examples, we'll apply random transformations to them each time we are looping over them. This way, we "augment" our training dataset to contain more data. There are various transformations available in `torchvision`, see [torchvision.transforms](https://pytorch.org/docs/stable/torchvision/transforms.html) for more information.
 
-
 input_image_size = (75, 75)
 
 data_transform = transforms.Compose([
@@ -82,6 +85,38 @@ noop_transform = transforms.Compose([
         transforms.ToTensor()
     ])
 
+
+# ### Data loaders
+# 
+# First we specify the pre-trained ViT model we are going to use. The model [`"google/vit-base-patch16-224"`](https://huggingface.co/google/vit-base-patch16-224) is pre-trained on ImageNet-21k (14 million images, 21,843 classes) at resolution 224x224, and fine-tuned on ImageNet 2012 (1 million images, 1,000 classes) at resolution 224x224.
+# 
+# We'll use a pre-trained ViT feature extractor that matches the ViT model to preprocess the input images. 
+
+VITMODEL = 'google/vit-base-patch16-224'
+model = ViTForImageClassification.from_pretrained(VITMODEL,
+                                                  num_labels=1,
+                                                  ignore_mismatched_sizes=True).to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-5)
+
+feature_extractor = ViTFeatureExtractor.from_pretrained(VITMODEL)
+
+
+# The we define a "collator" function. This is just a function passed to the `DataLoader` which will pre-process each batch of data. In our case we will pass the images through the `ViTFeatureExtractor` which will process the images into the correct format for ViT.
+
+class ImageClassificationCollator:
+    def __init__(self, feature_extractor):
+        self.feature_extractor = feature_extractor
+        
+    def __call__(self, batch):
+        x = self.feature_extractor([x[0] for x in batch], return_tensors='pt')
+        x['labels'] = torch.tensor([x[1] for x in batch], dtype=torch.float32)
+        return x
+
+collator = ImageClassificationCollator(feature_extractor)
+
+
+# Let's now define our data loaders for training, validation, and test data.
+
 class ImageFolderRemoveDirs(datasets.ImageFolder):
     def __init__(self, root, transform, remove_dirs):
         self.remove_dirs = remove_dirs
@@ -96,79 +131,35 @@ class ImageFolderRemoveDirs(datasets.ImageFolder):
         return classes, class_to_idx
 
 
-
-# ### Data loaders
-# 
-# Let's now define our real data loaders for training and validation data.
-
 batch_size = 50
 
 print('Train: ', end="")
 train_dataset = datasets.ImageFolder(root=datapath+'/train',
-                                     transform=data_transform)
+                                     transform=transforms.ToTensor())
 train_loader = DataLoader(train_dataset, batch_size=batch_size,
-                         shuffle=True, num_workers=4)
+                         shuffle=True, num_workers=4,
+                         collate_fn=collator)
 print('Found', len(train_dataset), 'images belonging to',
      len(train_dataset.classes), 'classes')
 
 print('Validation: ', end="")
 validation_dataset = ImageFolderRemoveDirs(root=datapath+'/validation',
-                                           transform=noop_transform,
+                                           transform=transforms.ToTensor(),
                                            remove_dirs=['00027', '00039'])
 validation_loader = DataLoader(validation_dataset, batch_size=batch_size,
-                         shuffle=False, num_workers=4)
+                              shuffle=False, num_workers=4,
+                              collate_fn=collator)
 print('Found', len(validation_dataset), 'images belonging to',
      len(validation_dataset.classes), 'classes')
 
 print('Test: ', end="")
 test_dataset = datasets.ImageFolder(root=datapath+'/test',
-                                     transform=noop_transform)
+                                     transform=transforms.ToTensor())
 test_loader = DataLoader(test_dataset, batch_size=batch_size,
-                         shuffle=False, num_workers=4)
+                         shuffle=False, num_workers=4,
+                        collate_fn=collator)
 print('Found', len(test_dataset), 'images belonging to',
      len(test_dataset.classes), 'classes')
-
-
-# ## Option 1: Train a small CNN from scratch
-# 
-# Similarly as with MNIST digits, we can start from scratch and train a CNN for the classification task. However, due to the small number of training images, a large network will easily overfit, regardless of the data augmentation.
-# 
-# ### Initialization
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, (3, 3))
-        self.pool1 = nn.MaxPool2d((2, 2))
-        self.conv2 = nn.Conv2d(32, 32, (3, 3))
-        self.pool2 = nn.MaxPool2d((2, 2))
-        self.conv3 = nn.Conv2d(32, 64, (3, 3))
-        self.pool3 = nn.MaxPool2d((2, 2))
-        self.fc1 = nn.Linear(7*7*64, 128)
-        self.fc1_drop = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, 43)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool1(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool2(x)
-        x = F.relu(self.conv3(x))
-        x = self.pool3(x)
-
-        # "flatten" 2D to 1D
-        x = x.view(-1, 7*7*64)
-        x = F.relu(self.fc1(x))
-        x = self.fc1_drop(x)
-        return self.fc2(x)
-
-model = Net().to(device)
-optimizer = optim.SGD(model.parameters(), lr=0.03)
-#optimizer = optim.RMSprop(model.parameters())
-criterion = nn.CrossEntropyLoss()
-
-print(model)
 
 
 # ### Learning
@@ -179,28 +170,31 @@ def train(epoch, scores=None):
     epoch_loss = 0
 
     # Loop over each batch from the training set
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, data in enumerate(train_loader):
         # Copy data to GPU if needed
-        data = data.to(device)
-        target = target.to(device)
+        data["pixel_values"] = data["pixel_values"].to(device)
+        data["labels"] = data["labels"].to(device)
 
         # Zero gradient buffers
         optimizer.zero_grad()
 
         # Pass data through the network
-        output = model(data)
+        output = model(**data)
 
-        loss = criterion(output, target)
-        epoch_loss += loss.data.item()
+        # output is actually a SequenceClassifierOutput object that contains
+        # the loss and classification scores (logits)
+
+        # add up the loss
+        epoch_loss += output.loss.data.item()
 
         # Backpropagate
-        loss.backward()
+        output.loss.backward()
 
         # Update weights
         optimizer.step()
 
     epoch_loss /= len(train_loader.dataset)
-    print('Train Epoch: {}, Loss: {:.4f}'.format(epoch, epoch_loss))
+    print('Train Epoch: {}, Loss: {:.6f}'.format(epoch, epoch_loss))
 
     if scores is not None:
         if 'loss' not in scores:
@@ -214,22 +208,23 @@ def train(epoch, scores=None):
 def evaluate(loader, scores=None, iteration=-1):
     model.eval()
     loss, correct = 0, 0
-    for data, target in loader:
-        data = data.to(device)
-        target = target.to(device)
+    for data in loader:
+        data["pixel_values"] = data["pixel_values"].to(device)
+        data["labels"] = data["labels"].to(device)
 
-        output = model(data)
+        output = model(**data)
 
-        loss += criterion(output, target).data.item()
+        loss += output.loss.data.item()
 
-        _, pred = output.max(1)
-        correct += pred.eq(target.data).cpu().sum()
+        pred = output.logits>0.5
+        lab = data["labels"].unsqueeze(1)
+        correct += pred.eq(lab).cpu().sum()
 
     loss /= len(loader.dataset)
 
     accuracy = 100. * correct.to(torch.float32) / len(loader.dataset)
 
-    print('Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    print('Average loss: {:.6f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         loss, correct, len(loader.dataset), accuracy))
 
     if scores is not None:
@@ -245,7 +240,7 @@ def evaluate(loader, scores=None, iteration=-1):
         log.add_scalar('val_acc', accuracy, iteration)
 
 
-epochs = 50
+epochs = 4
 
 train_scores = {}
 valid_scores = {}
@@ -255,13 +250,12 @@ for epoch in range(1, epochs + 1):
     train(epoch, train_scores)
 
     with torch.no_grad():
-        print('\nValidation:')
+        print('Validation:')
         evaluate(validation_loader, valid_scores, epoch-1)
-
 end_time = datetime.now()
 print('Total training time: {}.'.format(end_time - start_time))
 
-# torch.save("gtsrb-small-cnn.pt")
+# torch.save("gtsrb-small-cnn.pt")')
 
 # ### Inference
 
