02c4f4093a14efcf2a1b757adf00dac63163f1de
Shanshan Wang <shanshan.wang@csc.fi>
2026-03-02 12:19:50 +0200

    update the batch script using laif container

--- day2/run.sh
+++ day2/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --account=project_462001095
-#SBATCH --partition=small-g
+#SBATCH --account=project_462000131 # switch to the actual project
+#SBATCH --partition=small-g # switch to the actual partition
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=7
 #SBATCH --gpus-per-task=1
 #SBATCH --mem=60G
 #SBATCH --time=1:00:00
-#SBATCH --reservation=pdl-day2-no-ood
+##SBATCH --reservation=pdl-day2-no-ood # uncomment this for the actual course
 
 module purge
-module use /appl/local/csc/modulefiles/
-module load pytorch/2.4
+module use /appl/local/laifs/modules
+module load lumi-aif-singularity-bindings
+export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260124_092648/lumi-multitorch-full-u24r64f21m43t29-20260124_092648.sif
 
 COURSE_SCRATCH="/scratch/${SLURM_JOB_ACCOUNT}"
 
@@ -21,7 +22,10 @@ export HF_HOME=$COURSE_SCRATCH/hf-cache
 export MLFLOW_TRACKING_URI=$COURSE_SCRATCH/data/users/$USER/mlruns
 export TOKENIZERS_PARALLELISM=false
 
+export MIOPEN_DISABLE_CACHE=1
+export MIOPEN_USER_DB_PATH=""
 umask 002
 
 set -xv
-python3 $*
+srun singularity exec $SIF bash -c "source myvenv/bin/activate && python3 $*"
+
